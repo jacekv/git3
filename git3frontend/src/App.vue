de73869a0eacdaee671b01d7f11fbd381d2e2c51
jacekv <jaca347@gmail.com>
2020-10-13 20:26:09 +0200

    Some frontend :)

--- git3frontend/src/App.vue
+++ git3frontend/src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <NavigationBar/>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <v-row>
+          <v-col cols='8' offset='2'>
+            <FileExplorer/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import NavigationBar from './components/NavigationBar.vue';
+import FileExplorer from './components/FileExplorer.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld,
+    NavigationBar,
+    FileExplorer,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
